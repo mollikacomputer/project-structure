aae1cd1babd74dffb30430bcb46d9515c6e599d3
mollikacomputer <mollikacomputer3@gmail.com>
2022-09-13 21:10:13 +0600

    .env file structure added and some comment describe

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+// middleware
 app.use(express.json());
 app.use(cors());
 
+// server testing home page
 app.get('/', (req, res)=>{
     res.send('Project Structure app is running');
 });
